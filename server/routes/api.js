0f2fa0be97fa1d765c92c30f29f9bd2a03aa0b87
M_Kmail <kmailmohammad09@gmail.com>
2022-07-31 11:05:17 +0300

    bug fixed

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,33 +1,47 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const todos = []
-let id = 1
+const todos = [];
+let id = 1;
 
-router.get('/todos', function (req, res) {
-    res.send(todos)
-})
+router.get("/todos", function (req, res) {
+  res.send(todos);
+});
 
-router.post('/todo', function (req, res) {
-    const text = req.body.text
-    const newTodo = { id: id++, text: text, complete: false }
+router.post("/todo", function (req, res) {
+  const text = req.body.text;
+  const newTodo = { id: id++, text: text, complete: false };
 
-    todos.push(newTodo)
-    res.send(todos)
-})
+  todos.push(newTodo);
+  res.send(todos);
+});
 
-router.put('/todo/:todoID', function (req, res) {
-    const todoID = req.params.todoID
+router.put("/todo/:todoID", function (req, res) {
+  const todoID = req.params.todoID;
 
-    todos.find(t => t.id == todoID).completed = true
-    res.send(todos)
-})
+  todos.find((t) => t.id == todoID).complete = true;
+  res.send(todos);
+});
 
-router.delete('/todo/:todoID', function (req, res) {
-    const todoID = req.params.todoID
-    todos.splice(todoID, 1)
-    console.log('hello')
-    res.send(todos)
-})
+const delToDo = function (todoID) {
+  let pos = 0;
 
-module.exports = router
+  for (let i in todos) {
+    if (todos[i].id == todoID) {
+      pos = i;
+      break;
+    }
+  }
+
+  todos.splice(pos, 1);
+};
+
+router.delete("/todo/:todoID", function (req, res) {
+  const todoID = req.params.todoID;
+
+  delToDo(todoID);
+
+  res.send(todos);
+});
+
+module.exports = router;
